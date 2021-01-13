e671202f92127393cd03ba9f7f4c2c37372e9016
Samiur Prapon <samiur.prapon@gmail.com>
2021-01-14 00:56:07 +0600

    #8 User Profile UI added.

---
+++ app/src/main/java/life/nsu/sadchat/CreateProfileActivity.java
@@ -0,0 +1,14 @@
+package life.nsu.sadchat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}
--- app/src/main/java/life/nsu/sadchat/VerificationActivity.java
+++ app/src/main/java/life/nsu/sadchat/VerificationActivity.java
@@ -150,7 +150,7 @@ public class VerificationActivity extends AppCompatActivity {
     }
 
     private void route() {
-        Intent intent = new Intent(VerificationActivity.this, MainActivity.class);
+        Intent intent = new Intent(VerificationActivity.this, CreateProfileActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
         startActivity(intent);
